87cad48cc7cf8e633aba1a3ba84fc417f192ecf0
kawenzy <ahmadfakhri0211@gmail.com>
2024-10-12 00:56:08 +0700

    ipd: component 🍵

---
+++ src/app/home/jurnal/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return(
+        <div className="bg-paccent p-4 w-full">
+            <div className="flex flex-col gap-4">
+                <div className="grid grid-flow-col grid-cols-4 gap-4 items-center m-auto">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
